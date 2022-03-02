3ab4259407639fc1295eac29f8b242a5690ac033
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2022-03-01 21:28:21 -0700

    update wallet and blackjack

--- Blackjack.rb
+++ Blackjack.rb
@@ -1,6 +1,5 @@
 # require_relative 'Wallet.rb'
-require_relative 'Math.rb'
-# require_relative 'App.rb'
+# require_relative 'Math.rb'
 require_relative 'User.rb'
 
 class Blackjack
@@ -9,11 +8,12 @@ class Blackjack
 
   def initialize(user)
     @user = user
-    blackjack_menu
+    blackjack_main_menu
   end
 
-  def blackjack_menu
-    puts "Welcome to Blackjack"
+
+  def blackjack_main_menu
+    puts "-- Welcome to Blackjack --"
     puts "It's between you and the dealer"
     puts "Whomever gets closest to 21 wins"
     bj_game_menu
@@ -21,46 +21,130 @@ class Blackjack
 
 
   def bj_game_menu
-    @dealer_total = 0
-    @user_total = 0
-    until (@user_total >= 21 || @dealer_total >= 21) do
-      puts "Hit me? 'Y' or Fold 'N'"
-      user_choice = gets.strip.downcase
-        
-        if user_choice == 'n'
-          final_score
-        end
 
-      user_card = rand(1..10)
-      dealer_card = rand(1..10)
-      @user_total += user_card
-      @dealer_total += dealer_card
-      puts "You're card was #{user_card}"
-      puts "You're now at #{@user_total}"
-      puts "Your dealer is now at #{@dealer_total}"
-
-        if @user_total > 21
-          puts "You exceeded 21, You lose"
-        elsif @dealer_total > 21
-          puts "Dealer exceeded 21, You Win"
-        end 
-    end
-  end
-
-  def final_score
-    puts "You're at #{@user_total}"
-    puts "Dealer is at #{@dealer_total}"
-    if @user_total > @dealer_total
-      
-      puts "You win"
-    elsif @user_total < @dealer_total
-      puts "Dealer wins"
-    else @user_total == 0 && @dealer_total == 0
-      puts "noone wins, try again"
+    puts "Place your bet. or 'exit' to Exit"
+    @user_bet = gets.strip.to_f
+    if @user_bet == 0
       exit
     end
-    blackjack_menu
+    if @user.validate_money(@user_bet) != true
+      puts "You don't have that much in your wallet."
+      bj_game_menu
+    end
+    game_loop
+  end
+
+
+  def game_loop
+
+    @dealer_total = 0
+    @player_total = 0
+
+    while (@player_total <= 21 || @dealer_total <= 21) do
+
+      puts "Hit Me 'Y' - Hold 'N' - Fold 'F'"
+      user_choice = gets.strip.downcase
+      choices = ['y','n', 'f']
+        until choices.include?(user_choice)
+          puts "Try again"
+          user_choice = gets.strip.downcase
+        end
+        
+        if user_choice == 'f' 
+          final_score(@player_total, @dealer_total)
+          break
+        end
+  
+        if user_choice =='n' && @dealer_total <= 17 
+          dealer_card = rand(1..10)
+          @dealer_total += dealer_card
+          puts "Dealer card: #{dealer_card}"
+          final_score(@player_total, @dealer_total)
+          break
+        elsif user_choice =='n' && @dealer_total >= 18
+          puts "Dealer Held"
+          final_score(@player_total, @dealer_total)
+          break
+        end
+  
+        if @dealer_total > 18 && user_choice == 'y' 
+          player_card = rand(1..10)
+          @player_total += player_card
+          puts "You're card was: #{player_card}"
+          puts "Dealer Held"
+          final_score(@player_total, @dealer_total)
+          break
+        end
+
+
+
+      player_card = 21
+      dealer_card = rand(1..10)
+      @player_total += player_card
+      @dealer_total += dealer_card
+      puts "You're card was #{player_card}"
+      puts "You're now at #{@player_total}"
+      puts "Your dealer is now at #{@dealer_total}"
+
+      if @player_total == 21
+        puts "You hit 21!"
+        final_score(@player_total, @dealer_total)
+        break
+      elsif @dealer_total == 21
+        puts "Dealer hit 21!"
+        final_score(@player_total, @dealer_total)
+        break
+      end
+
+
+        if @player_total > 21
+          puts "* You exceeded 21 *" 
+          over_21(@player_total, @dealer_total)
+          break
+        elsif @dealer_total > 21
+          puts "* Dealer exceeded 21 *"
+          over_21(@player_total, @dealer_total)
+          break
+        end 
+
+    end
+    
+  end
+
+
+  def over_21(player, dealer)
+    if player > 21 && dealer > 21
+      puts "player: #{@player_total} -- dealer: #{@dealer_total}"
+      puts "Noone wins, Try again"
+    elsif player < 21 && dealer > 21
+      puts "You win: #{player}"
+      @user.game_win(@user_bet)
+      puts "You now have $#{@user.current_bal} in your wallet"
+    elsif dealer < 21 && player > 21
+      puts "Dealer wins: #{dealer}"
+      @user.game_lose(@user_bet)
+      puts "You now have $#{@user.current_bal} in your wallet"
+    end
+
+  end
+
+  def final_score(player, dealer)
+
+    if player > dealer && player <= 21
+      puts "You win: #{player}"
+      @user.game_win(@user_bet)
+      puts "You now have $#{@user.current_bal} in your wallet"
+    elsif dealer > player && dealer <= 21
+      # puts "Dealer card : #{@dealer_card}"
+      puts "Dealer wins: #{dealer}"
+      @user.game_lose(@user_bet)
+      puts "You now have $#{@user.current_bal} in your wallet"
+    elsif player === dealer
+      puts "It's a TIE! No one wins, Try again"
+    end
+  
   end
 
 end
+
 
--- Wallet.rb
+++ Wallet.rb
@@ -1,34 +1,30 @@
 
-class Wallet
-  def initialize(money)
-    @wallet = money
-  end
+# class Wallet
+# attr_accessor :user
 
-  def current_bal
-    @wallet
-  end
+#   def initialize(money)
+#     @wallet = money
+#   end
 
-  def validate_money(money)
-    if money > 0 && (@wallet - money) >= 0
-      true
-    else
-      false
-    end
-  end
+#   def current_bal
+#     @wallet
+#   end
 
-  
-
+#   def validate_money(money)
+#     if money > 0 && (@wallet - money) >= 0
+#       true
+#     else
+#       false
+#     end
+#   end
     
-  
 
-  def game_lose(money)
-    @wallet -= money
-  end
+#   def game_lose(money)
+#     @wallet -= money
+#   end
 
-  def game_win(money)
-  @wallet += money
-  end
+#   def game_win(money)
+#     @wallet += money
+#   end
 
-
-
-end
+# end
